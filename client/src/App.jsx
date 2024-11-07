f5116cda626dc04cb966b15b48a02632f44a0c4b
vgdhieu0602 <vg.dhieu0602@gmail.com>
2024-11-07 20:45:18 +0700

    setup base client project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
-
+import { Routes, Route, Navigate } from "react-router-dom"
+import Chat from './pages/Chat'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
     <>
-      Chat App
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </>
   )
 }
