993649ab5e79f6a7186d7913a132e56d68fce538
vgdhieu0602 <vg.dhieu0602@gmail.com>
2024-12-09 00:51:40 +0700

    Finish folder structure

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,7 +3,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button>Hello world</Button>
+      <Button variant={"outline"} className="font-bold text-indigo-500">
+        Hello world
+      </Button>
     </div>
   );
 }
